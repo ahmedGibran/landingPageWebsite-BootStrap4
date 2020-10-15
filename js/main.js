141ecef29f3b516d908088f04d6cefae698e6e3d
n <mac@MBPalkhasbMac.lan>
2020-10-16 00:12:19 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+$(".homeburger-menu").on("click",function(){
+    $(".toggle").toggleClass('open');
+    $(".nav-list").toggleClass('open');
+
+});
+
+AOS.init({
+        easing:'ease',
+        duration:1000
+});
+});
